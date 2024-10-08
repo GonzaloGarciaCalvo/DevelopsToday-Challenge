1eea7b29bd8acb2329bbcc3a4420514dcd2a1144
Gonzalo <garciacalvog@hotmail.com>
2024-10-08 16:05:45 -0300

    suspense in [country]

--- frontend/src/app/[country]/page.js
+++ frontend/src/app/[country]/page.js
@@ -43,59 +43,62 @@ export default function Page({ params }) {
   };
 
   return (
-    
-    <div>
-      <section className='flex flex-row justify-center py-4 '>
-        {
-          loading ? 
-          <div className='flex flex-col justify-center items-center min-h-screen'>
-            <h1 className='text-4xl p-10'>Loading</h1>
-            <div className=' animate-ping bg-slate-100 rounded-full h-16 w-16'></div>
-          </div> 
-          
-          : null
-        }
-        {
-          countryData? 
-          <>
-            <h1 className='text-4xl px-3'>{country}</h1>
-            {countryData?.flag 
-              && 
-              <Image 
-                src={countryData.flag} 
-                alt={`flag of ${country}`} 
-                width={45} height={30} priority 
-              />
-            }
-            
-          </>
-          : null
-        }
-      </section>
-      <section className='px-2 md:px-8 xl:px-2'>
-        {countryData?.borders && <h2 className='text-2xl "px-1 xl:py-18 px-24"'>Border Countries</h2>}
-        <div className='flex flex-row'>
+    <Suspense> 
+
+
+      <div>
+        <section className='flex flex-row justify-center py-4 '>
           {
-            countryData?.borders?.map((border) => {
-              return (
-                <div 
-                  key={border.commonName} 
-                  className='bg-slate-700 min-w-20 p-1 m-2 rounded-md flex justify-center hover:bg-red-950'
-                >
-                  <Link href={`/${border.commonName}?code=${border.countryCode}`}> 
-                    {border.commonName}
-                  </Link>
-                </div>
-              )
-            })
+            loading ? 
+            <div className='flex flex-col justify-center items-center min-h-screen'>
+              <h1 className='text-4xl p-10'>Loading</h1>
+              <div className=' animate-ping bg-slate-100 rounded-full h-16 w-16'></div>
+            </div> 
+            
+            : null
           }
-        </div>
-      </section>
-      <section className='p-1 md:p-8 xl:p-24' >
-        {
-          countryData?.population?.length && <LineGraf lineChartData={lineChartData} />
-        }
-      </section>
-    </div>
+          {
+            countryData? 
+            <>
+              <h1 className='text-4xl px-3'>{country}</h1>
+              {countryData?.flag 
+                && 
+                <Image 
+                  src={countryData.flag} 
+                  alt={`flag of ${country}`} 
+                  width={45} height={30} priority 
+                />
+              }
+              
+            </>
+            : null
+          }
+        </section>
+        <section className='px-2 md:px-8 xl:px-2'>
+          {countryData?.borders && <h2 className='text-2xl "px-1 xl:py-18 px-24"'>Border Countries</h2>}
+          <div className='flex flex-row'>
+            {
+              countryData?.borders?.map((border) => {
+                return (
+                  <div 
+                    key={border.commonName} 
+                    className='bg-slate-700 min-w-20 p-1 m-2 rounded-md flex justify-center hover:bg-red-950'
+                  >
+                    <Link href={`/${border.commonName}?code=${border.countryCode}`}> 
+                      {border.commonName}
+                    </Link>
+                  </div>
+                )
+              })
+            }
+          </div>
+        </section>
+        <section className='p-1 md:p-8 xl:p-24' >
+          {
+            countryData?.population?.length && <LineGraf lineChartData={lineChartData} />
+          }
+        </section>
+      </div>
+    </Suspense>
   )
 }
